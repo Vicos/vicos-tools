d852dbd4e2ae71b35c62b6ce7c7aba5df1411980
Vicos <vicos51@gmail.com>
2013-02-09 19:58:56 +0100

    Add routes to shared directories

--- http-sharing/app.js
+++ http-sharing/app.js
@@ -5,22 +5,33 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
 var app = express();
 
+var sharedDirs = [
+   { name: 'visdur2', path: 'E:/' },
+];
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('shared dirs', sharedDirs);
+  
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.directory(path.join(__dirname, 'public')));
+  
+  for (var i in sharedDirs) {
+    app.use('/shared/'+sharedDirs[i].name, express.static(sharedDirs[i].path));
+    app.use('/shared/'+sharedDirs[i].name, express.directory(sharedDirs[i].path));
+  }
 });
 
 app.configure('development', function(){
@@ -28,7 +39,6 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
